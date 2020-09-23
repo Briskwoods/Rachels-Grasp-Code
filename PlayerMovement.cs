a3980127230819a3d4889c9e247a6d25ca2fdc5c
Jeff Gichuki <julian.gichuki@gmail.com>
2020-09-23 13:34:58 +0300

    Initial commit

---
+++ PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+
+    public float cooldownTime = 2f;
+    private float nextDashTime = 0f;
+    
+    bool jump = false;
+    bool crouch = false;
+    bool dash = false;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Horizontal Movement Detection
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+       
+        //Detect Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        //Detect Crouch
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+        //Dash Ability
+        if (Time.time > nextDashTime) //Dash Ability cooldown 
+        {
+            if (Input.GetButtonDown("Dash"))
+            {
+                nextDashTime = Time.time + cooldownTime;
+                dash = true;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Move Character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump, dash);
+        jump = false;
+        dash = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Rachels-Grasp-Code
+ C# scripts For my 2D game called Rachel's Grasp.
