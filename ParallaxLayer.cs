52e54978b74bb50795871e5e33c2940cc7643611
Jeff Gichuki <julian.gichuki@gmail.com>
2020-09-30 01:03:09 +0300

    Parallax Effect on the background.
    This script adds a perspective parallax effect onto the background, *not onto the camera*.

---
+++ ParallaxLayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Platformer.View
+{
+    /// <summary>
+    /// Used to move a transform relative to the main camera position with a scale factor applied.
+    /// This is used to implement parallax scrolling effects on different branches of gameobjects.
+    /// </summary>
+    public class ParallaxLayer : MonoBehaviour
+    {
+        [SerializeField] private Vector2 parallaxEffectMultiplier;
+        private Transform cameraTransform;
+        private Vector3 lastCameraPosition;
+        //private float textureUnitSizeX;                             // Used in the infinite Scroll
+
+        void Start()
+        {
+            //Initialization of key variables
+            cameraTransform = Camera.main.transform;
+            lastCameraPosition = cameraTransform.position;
+            Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+            Texture2D texture = sprite.texture;
+           //textureUnitSizeX = texture.width / sprite.pixelsPerUnit;   // Used for the infinite scroll
+        }
+
+        void LateUpdate()
+        {
+            //Parallax Camera Implementation            
+            Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+            transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+            lastCameraPosition = cameraTransform.position;
+            
+            /* This is code to control the infinite scroll capability however this is for a much later implementation of this works, in the optimization phase*/
+            //if(Mathf.Abs(cameraTransform.position.x - transform.position.x)>= textureUnitSizeX)
+            //{
+            //    float offsetPositionX = (6 * cameraTransform.position.x - 6 * transform.position.x)%textureUnitSizeX;
+            //    transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, cameraTransform.position.y);
+            //}
+        
+        }
+
+    }
+}
