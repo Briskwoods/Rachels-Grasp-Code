281f8356806e75d7b5ca855fa610358a55d1429c
Jeff Gichuki <julian.gichuki@gmail.com>
2020-10-17 11:18:46 +0300

    Respawn and Ckeckpoint
    Respawn Gate and Checkout control the functioning of the respawn point and the player checkpoints

---
+++ Checkpoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private GameManager gameManager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameManager.lastCheckpointPos = transform.position;
+        }
+    }
+}
+
---
+++ RespawnGate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnGate : MonoBehaviour
+{
+    [SerializeField] public GameManager gameManager;
+
+    private void OnTriggerEnter2D(Collider2D gate)
+    {
+        if (gate.CompareTag("Player"))
+        {
+            gameManager.onRespawn();
+        }
+    }
+}
