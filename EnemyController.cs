76f28958518b1356023645e2d4bc3ea7b04651ee
Jeff Gichuki <julian.gichuki@gmail.com>
2020-10-18 21:42:26 +0300

    Enemy Controller functionality fully fixed
    The enemy can now attack the player with the hitplayer variable constantly checking if the player is hit. It can also be used on multiple enemy game objects

--- EnemyController.cs
+++ EnemyController.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private int m_maxHealth = 100;                     // Enemy max health variable.
-    [SerializeField] private int m_currentHealth;                       // Enemy current Health variable
+    [SerializeField] private int m_currentHealth = 0;                   // Enemy current Health variable
+    [SerializeField] public int m_attackDamage = 0;                     // How much damage the enemy does to the player
+
     [SerializeField] private float m_attackRange = 0.5f;                // Enemy attack range.
-    [SerializeField] private float m_patrolSpeed;                       // Patrol Speed of the enemy.
+    [SerializeField] private float m_patrolSpeed = 1f;                  // Patrol Speed of the enemy.
     [SerializeField] private float m_groundCheckDistance;               // Height for ground check distance when patrolling on a platform.
     [SerializeField] private float m_wallCheckRadius;                   // Radius of wall check cicrcle collider when patrolling on ground
-    [SerializeField] private float m_moveSpeed;                         // Speed at which enemy moves towards the player after player detection
+    [SerializeField] private float m_moveSpeed = 2f;                    // Speed at which enemy moves towards the player after player detection
     [SerializeField] private float m_playerDetectRadius;                // Enemy radius to detect if player is in attacking range
+    [SerializeField] private float m_nextAttackTime = 0f;               // Time Until Next Attack
+    [SerializeField] private float m_TimeBetweenAttacks = 1.5f;
+    [SerializeField] private float m_stoppingDistance =1f;              // Distance at which the enemy stops before the player.
+    [SerializeField] private float m_knockbackForceX =100f;
+    [SerializeField] private float m_knockbackForceY = 100f;
 
+    [SerializeField] private PlayerHealthManager playerHealth;          // For interactions with the player health manager
+    [SerializeField] private Rigidbody2D m_rigidbody;                   // For Knockback force
+    
     [SerializeField] private Transform m_lineOfSightStart;              // Where the enemies Line of sight begins
     [SerializeField] private Transform m_lineOfSightEnd;                // Where the enemies Line of sight ends
     [SerializeField] private Transform m_attackPoint;                   // Where the enemy attack lands
@@ -30,49 +41,58 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private bool m_isGround;                           // Variable used to check if the enemy is patrolling on a platform or on the ground
     
     private bool playerSeen;                                            // Set to true or false when enemy detects player
-    private bool wallInfo;
+    private bool wallInfo;                                              // Detects the precense of a wall.
     private bool m_movingRight = true;                                  // Used in flipping the enemy sprite when the enemy is turning
-    private bool inRange;
-
-    private int m_numberOfTimesPlayerIsDetected = 0;
+    private bool inRange;                                               // Detects if player is in range.
+    private bool hitPlayer;
+        
+    private int m_numberOfTimesPlayerIsDetected = 0;                    
     
     // Start is called before the first frame update
     void Start()
     {
+        // Set current health to max on start
         m_currentHealth = m_maxHealth;
+
+        // Initialise specified variables
         m_target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        
+        // Sets the number of times a player is detected to 0, this allows us to have idle enemies waiting to detect the player and move.
+        m_numberOfTimesPlayerIsDetected = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Checks if player has been hit
+        hitPlayer = Physics2D.OverlapCircle(m_attackPoint.position, m_attackRange, m_PlayerLayer);
+        switch (hitPlayer)
+        {
+            case true:
+                switch (Time.time > m_nextAttackTime)
+                {
+                    case true:
+                        playerHealth.TakeDamage(m_attackDamage);
+                        // Attack Cooldown
+                        m_nextAttackTime = Time.time + m_TimeBetweenAttacks;
+                        break;
+                    case false:
+                        break;
+                }
+                break;
+            case false:
+                break;
+        }
+
+
         //This line prevents the raycast from detecting the enemy's box colliders
         Physics2D.queriesStartInColliders = false;
         //We use a Linecast to detect the presence of a player and if found the enemy can move towards the player.
-        playerSeen = Physics2D.Linecast(m_lineOfSightStart.position, m_lineOfSightEnd.position,m_PlayerLayer);       
-        Debug.DrawLine(m_lineOfSightStart.position, m_lineOfSightEnd.position, new Color(1f,1f,1f));
-
-        switch (playerSeen)
-        {
+        playerSeen = Physics2D.Linecast(m_lineOfSightStart.position, m_lineOfSightEnd.position, m_PlayerLayer);
+        Debug.DrawLine(m_lineOfSightStart.position, m_lineOfSightEnd.position, new Color(1f, 1f, 1f));
+        // If the player in not Detected
+        switch(!playerSeen){
             case true:
-                // Used in setting the enemy to patrol after player detection
-                m_numberOfTimesPlayerIsDetected = 1;
-                // Animate React
-                m_animator.SetTrigger("SeePlayer");
-                // Sets Patrolling to false to prevent clash.
-                m_isPatrolling = false;
-                //Move to player
-                transform.position = Vector2.MoveTowards(transform.position, new Vector2(m_target.transform.position.x, transform.position.y), m_moveSpeed * Time.deltaTime);
-                break;
-            case false:
-                // Sets patrolling back to true after player detection.
-                switch (m_numberOfTimesPlayerIsDetected == 1) {
-                    case true:
-                        m_isPatrolling = true;
-                        break;
-                    case false:
-                        break;        
-                }
                 // Checks if the enemy is patrolling
                 switch (m_isPatrolling)
                 {
@@ -81,13 +101,13 @@ public class EnemyController : MonoBehaviour
                         transform.Translate(Vector2.right * m_patrolSpeed * Time.deltaTime);
                         //Animate the movement
                         m_animator.SetFloat("WalkSpeed", Mathf.Abs(m_patrolSpeed));
-                        // Checks if the enemy is on the ground or not. If they're not on the ground then they're on a floating platform.
+                        // Checks if the enemy is on the ground or not. If they're not on the ground then they're on a platform.
                         switch (m_isGround)
                         {
                             case true:
-                            // We use a sphere collider to detect the presence of a wall and if found, the enemy can turn as they patrol.
-                            wallInfo = Physics2D.OverlapCircle(m_wallDetection.position, m_wallCheckRadius,m_groundLayer);
-                            // If a wall is detected then we turn the player around.
+                                // We use a sphere collider to detect the presence of a wall and if found, the enemy can turn as they patrol.
+                                wallInfo = Physics2D.OverlapCircle(m_wallDetection.position, m_wallCheckRadius, m_groundLayer);
+                                // If a wall is detected then we turn the player around.
                                 switch (wallInfo)
                                 {
                                     case true:
@@ -106,7 +126,9 @@ public class EnemyController : MonoBehaviour
                                     case false:
                                         break;
                                 }
+                                // Used in detecting edges to prevent enemy from falling off the map.
                                 RaycastHit2D GroundInfo = Physics2D.Raycast(m_groundDetection.position, Vector2.down, m_groundCheckDistance);
+                                // If ground is not detected it changes to the platform script
                                 switch (!GroundInfo)
                                 {
                                     case true:
@@ -120,7 +142,7 @@ public class EnemyController : MonoBehaviour
                                 // In this case, when isGround is false that means the enemy is on a platform therefore we use a downwards raycast to detect where the plaform stops to turn the enemy
                                 RaycastHit2D groundInfo = Physics2D.Raycast(m_groundDetection.position, Vector2.down, m_groundCheckDistance);
                                 Debug.DrawRay(m_groundDetection.position, Vector2.down * transform.localScale.x * m_groundCheckDistance, new Color(1f, 1f, 1f));
-                                //If there is no ground information
+                                //If there is no ground information we can turn and patrol in the opposite direction
                                 switch (!groundInfo)
                                 {
                                     case true:
@@ -159,51 +181,104 @@ public class EnemyController : MonoBehaviour
                         m_animator.SetFloat("WalkSpeed", Mathf.Abs(0));
                         break;
                 }
+                switch (m_numberOfTimesPlayerIsDetected >= 1)
+                {
+                    case true:
+                        m_isPatrolling = true;
+                        break;
+                    case false:
+                        break;
+                }
                 break;
+            case false:
+                // If a player is detected, then the counter increases by one. This counter sets the enemy to constantly patrol after seeing the player.
+                m_numberOfTimesPlayerIsDetected += 1;
+                switch (m_isPatrolling){
+                    case true:
+                        // Sets is patrolling to false if a player is detected.
+                        m_isPatrolling = false;
+                        break;
+                    case false:
+                        // If patrolling is false then the enemy moves towards the player.
+                        // We use stopping distance to determine where the enemy stops to prevent collision with the player
+                        switch(Vector2.Distance(transform.position, m_target.position)>m_stoppingDistance){
+                            case true:
+                                // Moves enemy towards player if the distance to player is greater than 1
+                                transform.position  = Vector2.MoveTowards(transform.position, m_target.position, m_moveSpeed * Time.deltaTime);
+                                // Animate the movement
+                                m_animator.SetFloat("WalkSpeed", m_moveSpeed);
+                                break;
+                            case false:
+                               //Sets the enemy to stop in front of the player
+                            transform.Translate(Vector2.right * 0 * Time.deltaTime);
+                            //Animate the movement
+                            m_animator.SetFloat("WalkSpeed", Mathf.Abs(0));
+                            break; 
+                        }
+                        break;   
+                }
+            break;
         }
-
-        // We use a sphere collider to detect the presence of a wall and if found, the enemy can turn as they patrol.
+        // Checks if player is in attacking range for attacking enemies.
         inRange = Physics2D.OverlapCircle(m_playerAttackRange.position, m_playerDetectRadius, m_PlayerLayer);
+       
 
-        switch(inRange)
+        // If a player is in range
+        switch (inRange)
         {
             case true:
-                Debug.Log("Attacked");
+                // The enemy attacks the player.
+                // Enemy Attack cooldown 
+                switch (Time.time > m_nextAttackTime)
+                {
+                    case true:
+                        Attack();
+                        // Check if attack point hits the player
+                        // Attack Cooldown
+                        m_nextAttackTime = Time.time + m_TimeBetweenAttacks;
+                        break;
+                    case false:
+                        break;
+                }
                 break;
             case false:
                 break;
-                
         }
-
     }
+     
 
-    private void FixedUpdate()
-    {
-
-
-    }
-
+    // Enemy takes damage after attack from player
     public void TakeDamage(int damage)
     {
         m_currentHealth -= damage;
         m_animator.SetTrigger("Hurt");
+
+        if (m_target.transform.position.x > transform.position.x)
+        {
+            m_rigidbody.AddForce(transform.up * m_knockbackForceY + transform.right * -m_knockbackForceX);
+        }
+        else if (m_target.transform.position.x < transform.position.x)
+        {
+            m_rigidbody.AddForce(transform.up * m_knockbackForceY + transform.right * m_knockbackForceX);
+        }
 
         if (m_currentHealth <= 0)
         {
             Die();
         }
     }
-
+    // Enemy Attack Function
     void Attack()
     {
-
+        m_animator.SetTrigger("Attack");
+       
     }
-
+    // Enemy Death function
     void Die()
     {
         //Death animation
         m_animator.SetBool("isDead", true);
-        
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
         //Disable the enemy collider on death and prevent the script from continuing
         GetComponent<Collider2D>().enabled = false;
         this.enabled = false;
