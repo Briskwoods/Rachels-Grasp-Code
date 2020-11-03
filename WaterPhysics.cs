d4d0430a1a5cfc8b0fa35bae6f57e202410a7444
Jeff Gichuki <julian.gichuki@gmail.com>
2020-11-03 17:51:54 +0300

    Added Water Physics for testing
    Testing Water Physics

---
+++ WaterPhysics.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPhysics : MonoBehaviour
+{
+    private GameManager gameManager;
+    private GameObject Player;
+
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+}
