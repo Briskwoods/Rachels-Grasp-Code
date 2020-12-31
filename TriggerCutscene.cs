3b5b3bea7254946a94ba4c1e2bcf40bd7c7a9b35
Jeff Gichuki <julian.gichuki@gmail.com>
2020-12-31 13:28:06 +0300

    Added Trigger Script
    Triggers a cutscene when interacted with

---
+++ TriggerCutscene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TriggerCutscene : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector cutscene;
+    [SerializeField] private GameObject closingCutscene;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            closingCutscene.SetActive(true);
+            cutscene.Play();
+        }
+    }
+}
