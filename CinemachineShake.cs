819bb31b500b8ca4229563520376d5fe8f609f0c
Jeff Gichuki <julian.gichuki@gmail.com>
2020-12-31 13:10:14 +0300

    Added CinemachineShake.cs
    A script that controls / triggers the camera shake and stops the camera shake. Used with signals in the timeline/cutscenes.

---
+++ CinemachineShake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachineShake : MonoBehaviour
+{
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+    [SerializeField] private float shakeIntensity = 5f;
+
+
+    private void Awake()
+      {
+        cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public void ShakeCamera()
+    {
+        CinemachineBasicMultiChannelPerlin channelPerlin = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        channelPerlin.m_AmplitudeGain = shakeIntensity;
+    }
+
+    public void StopShake()
+    {
+        CinemachineBasicMultiChannelPerlin channelPerlin = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        channelPerlin.m_AmplitudeGain = 0f;
+    }
+}
