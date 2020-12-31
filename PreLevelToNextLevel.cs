ee0f92dd3aaa4dc1dc91d09cdbdb47411694d41c
Jeff Gichuki <julian.gichuki@gmail.com>
2020-12-31 13:30:28 +0300

    Added Trigger Script
    Loads the next scene when interacted with.

---
+++ PreLevelToNextLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreLevelToNextLevel : MonoBehaviour
+{
+    [SerializeField] public GameManager gameManager;
+
+    private void OnTriggerEnter2D(Collider2D zone)
+    {
+        if (zone.CompareTag("Player"))
+        {
+            gameManager.LoadNextScene();
+        }
+    }
+}
