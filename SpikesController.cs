d38cc34b0ed2a1194c616fefa486b3515a1be754
Jeff Gichuki <julian.gichuki@gmail.com>
2020-12-31 13:26:10 +0300

    Added SpikeController.cs
    Added spikes tilemaps into the game and control their damage output with this script.

---
+++ SpikesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikesController : MonoBehaviour
+{
+    [SerializeField] private PlayerHealthManager playerHealth;
+    [SerializeField] private int spikeDamage;
+    [SerializeField] private Rigidbody2D m_player;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Player")
+        {
+            playerHealth.TakeDamage(spikeDamage);
+
+            // Knockback the player
+            if (transform.position.x > m_player.position.x)
+            {
+                m_player.AddForce(transform.up * playerHealth.m_knockbackForceY + transform.right * (playerHealth.m_knockbackForceX));
+            }
+            else if (transform.position.x < m_player.transform.position.x)
+            {
+                m_player.AddForce(transform.up * playerHealth.m_knockbackForceY + transform.right * playerHealth.m_knockbackForceX);
+            }
+        }
+    }
+}
