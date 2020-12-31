f166251608845b8b505c6ebacf00bf87f00901f7
Jeff Gichuki <julian.gichuki@gmail.com>
2020-12-31 13:13:40 +0300

    Updated Dialogue system
    Made the name string an array to hold conversations with more than one character.

--- Dialogue.cs
+++ Dialogue.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 [System.Serializable]
 public class Dialogue
 {
-    public string name;
+    // Use an array to allow for multiple names to be used in the dialogue box to simulate a conversation with multiple people.
+    // If the array size is left to 1, then it will use that 1 name throughout the whole conversation
+    // If the array size is over 3, do not leave the any value in the array  blank as it will show up as a blank in the name field of the dialogue box.
+    // Multi-use innit?
+    public string[] name;
 
     [TextArea(3, 10)]
     public string[] sentences;
