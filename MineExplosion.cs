bb201c53a81166b6418c2da5b2690caf68d29d15
Jeff Gichuki <julian.gichuki@gmail.com>
2020-12-31 13:29:21 +0300

    Added Trigger Script
    Triggers an explosion and knocks the player back when interacted with.

---
+++ MineExplosion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineExplosion : MonoBehaviour
+{
+    [SerializeField] private PlayerHealthManager m_playerHealth;
+
+    [SerializeField] private int m_damage;
+
+    [SerializeField] private GameObject explosion;
+
+    [SerializeField] private Rigidbody2D m_player;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Destroy(gameObject);
+            Instantiate(explosion, gameObject.transform.position, gameObject.transform.rotation);
+            m_playerHealth.TakeDamage(m_damage);
+
+            // Knockback the player
+            if (transform.position.x > m_player.position.x)
+            {
+                m_player.AddForce(transform.up * m_playerHealth.m_knockbackForceY + transform.right * (m_playerHealth.m_knockbackForceX));
+            }
+            else if (transform.position.x < m_player.transform.position.x)
+            {
+                m_player.AddForce(transform.up * m_playerHealth.m_knockbackForceY + transform.right * m_playerHealth.m_knockbackForceX);
+            }
+        }
+        
+    }
+}
