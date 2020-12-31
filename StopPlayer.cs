87c0e62d6b8fbd1ef05b4662de771dd0802f14e6
Jeff Gichuki <julian.gichuki@gmail.com>
2020-12-31 13:28:26 +0300

    Added Trigger Script
    Triggers the stop player functionality in the game manager

---
+++ StopPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopPlayer : MonoBehaviour
+{
+    [SerializeField] private GameManager gameManager;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch(collision.tag == "Player")
+        {
+            case true:
+                gameManager.StopPlayer();
+                break;
+            case false:
+                break;
+        }
+    }
+}
