1aa75390740c6a98a7031d635245d53aa8ee7e61
Jeff Gichuki <julian.gichuki@gmail.com>
2020-12-31 13:09:08 +0300

    Added BreathingZone.cs
    Contains scripts for zones where player can breath when underwater.
    
    Uncommented code, may update in final

---
+++ BreathingZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreathingZone : MonoBehaviour
+{
+    [SerializeField] private PlayerHealthManager playerHealth;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag == "Player")
+        {
+            case true:
+                playerHealth.m_holdBreath = 30f;
+                break;
+            case false:
+                break;
+        }
+    }
+}
