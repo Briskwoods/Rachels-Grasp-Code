5eb807b1246ccfdff777eab0b27dcf13c7038e8d
Jeff Gichuki <julian.gichuki@gmail.com>
2021-06-28 12:57:59 +0300

    Added Room Trigger
    Added a trigger script to switch the cameras when a player enters a Platformer level room/zone.

---
+++ RoomTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject m_mainCamera;
+    [SerializeField] private GameObject m_roomCamera;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        switch(other.tag == "Player")
+        {
+            case true:
+                m_mainCamera.SetActive(false);
+                m_roomCamera.SetActive(true);
+                break;
+            case false:
+                break;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        switch (other.tag == "Player")
+        {
+            case true:
+                m_mainCamera.SetActive(true);
+                m_roomCamera.SetActive(false);
+                break;
+            case false:
+                break;
+        }
+    }
+}
