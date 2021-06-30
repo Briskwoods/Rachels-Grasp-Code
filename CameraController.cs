cc5331d57740f8741b210fded9fac4029c6757fc
Jeff Gichuki <julian.gichuki@gmail.com>
2021-07-01 01:16:38 +0300

    Added Basic Camera Controller
    Pauses the game during blends to allow for smooth camera transitions in scenes

---
+++ CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private CinemachineBrain brain;
+
+    private void Update()
+    {
+       switch (brain.IsBlending)
+        {
+            case true:
+                Time.timeScale = 0;
+                break;
+            case false:
+                Time.timeScale = 1;
+                break;
+        } 
+    }
+}
